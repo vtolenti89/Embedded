068764a99c9e38b342c6923a62690394ac0c4243
Victor Tolentino <votolentino@gmail.com>
2018-09-17 14:03:25 +0100

    Project restructured

--- CameraSystem/CameraSystem/Debug/src/wlan.d
+++
@@ -1,6 +0,0 @@
-src/wlan.d src/wlan.o: ../src/wlan.cpp ../src/wlan.h \
- ../src/../Protocols/uart.h
-
-../src/wlan.h:
-
-../src/../Protocols/uart.h:
--- CameraSystem/CameraSystem/src/display.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * display.cpp
- *
- * Created: 22.05.2017 15:39:45
- *  Author: vito7253
- */ 
-#include "display.h"
